0d744edf2b787c41916346a2b30acc64c232302a
Leslie Sage <leslie.sage@gmail.com>
2019-03-25 14:14:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ Someone's place in line doesn't match their ticket number.
 var tN = 0
 var lN = 0
 function newTicketMachine (line) {
-    line.push(tN++)
+    line.push(++tN)
 return `You are ticket number ${tN} and number ${line.length} in line.`
 } 
